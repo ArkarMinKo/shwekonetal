d34ec723186288b02ddb204001ee006ff879e98a
Arkar Min Ko <arkarminko27@gmail.com>
2025-10-30 18:45:19 +0630

    Update getTimesSalesByToday

--- routes/goldPrices.js
+++ routes/goldPrices.js
@@ -240,7 +240,7 @@ function getOpenStock(req, res){
           };
         });
 
-        const totalString = `၁ ကျပ်သားပေါက်စျေး ${toMyanmarNumber(buying_prices)} ကျပ် ≈ စုစုပေါင်း ${toMyanmarNumber(total_profit)} ကျပ်`
+        const totalString = `${toMyanmarNumber(total_profit)} ကျပ်`
 
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ success: true, total: totalString, data: formattedRows }));
