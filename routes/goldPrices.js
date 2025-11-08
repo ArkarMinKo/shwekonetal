7853c7c1a65259ab29e31563fc38824c1569d02c
Arkar Min Ko <arkarminko27@gmail.com>
2025-11-08 13:34:24 +0630

    Update Dashboard

--- routes/goldPrices.js
+++ routes/goldPrices.js
@@ -519,7 +519,7 @@ function getLatestSellingPrice(req, res) {
 
 // --- Get All Buying Prices (Formatted by Date & Nearest Hour Slot) ---
 function getAllBuyingPrices(req, res) {
-  const sql = "SELECT * FROM buying_prices ORDER BY date ASC, time ASC";
+  const sql = "SELECT * FROM selling_prices ORDER BY date ASC, time ASC";
   db.query(sql, (err, results) => {
     if (err) {
       res.statusCode = 500;
